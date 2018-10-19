6bc63677bc112b01c3cee44958f0f3d82419d6cd
xaota <xaota@users.noreply.github.com>
2018-10-20 00:30:14 +0500

    Поле ввода InputCounter

--- pages/input/page-input.js
+++ pages/input/page-input.js
@@ -7,6 +7,7 @@ import '../../views/source/view-source.js';
 import '../../views/import/view-import.js';
 
 import '/material/components/input/material-input.js';
+import '/material/components/input-count/material-input-count.js';
 import '/material/components/textarea/material-textarea.js';
 
 const component = Component.meta(import.meta.url, 'page-input');
