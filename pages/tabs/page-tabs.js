3336e1c737a59937efbbcf50151aacf03864224c
xaota <xaota@users.noreply.github.com>
2018-10-22 10:27:47 +0500

    Вкладки | MaterialTabs

--- pages/tabs/page-tabs.js
+++ pages/tabs/page-tabs.js
@@ -6,6 +6,7 @@ import '../../views/article/view-article.js';
 import '../../views/source/view-source.js';
 
 import '/material/components/tabs/material-tabs.js';
+import '/material/components/tabs-item/material-tabs-item.js';
 
 const component = Component.meta(import.meta.url, 'page-tabs');
 /**
