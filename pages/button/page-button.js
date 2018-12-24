c841f559b3273ef3853e77aa939c4f463c19af27
xaota <xaota@users.noreply.github.com>
2018-12-25 00:32:23 +0500

    Кнопки с иконками и подсказками

--- pages/button/page-button.js
+++ pages/button/page-button.js
@@ -8,6 +8,9 @@ import '../../views/import/view-import.js';
 
 import '/material/components/button/material-button.js';
 import '/material/components/button-back/material-button-back.js';
+import '/material/components/button-icon/material-button-icon.js';
+import '/material/components/button-tooltip/material-button-tooltip.js';
+import '/material/components/button-tooltip-icon/material-button-tooltip-icon.js';
 
 import '/material/components/icon/material-icon.js';
 
