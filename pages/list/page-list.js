1ba8db829c10464b9f89eea3a775c4b80d6236b0
xaota <1172619+xaota@users.noreply.github.com>
2019-11-04 18:49:33 +0500

    создание списка через JS

--- pages/list/page-list.js
+++ pages/list/page-list.js
@@ -6,8 +6,10 @@ import '../../views/article/view-article.js';
 import '../../views/source/view-source.js';
 import '../../views/import/view-import.js';
 
-import '/material/components/list/material-list.js';
-import '/material/components/list-item/material-list-item.js';
+import MaterialList from '/material/components/list/material-list.js';
+import MaterialListItem from '/material/components/list-item/material-list-item.js';
+
+import '/material/components/button/material-button.js';
 
 const component = Component.meta(import.meta.url, 'page-list');
 /**
@@ -19,6 +21,20 @@ const component = Component.meta(import.meta.url, 'page-list');
     */
     constructor() {
       super(component);
+    }
+
+  /** */
+    mount(node) {
+      const button = node.querySelector('material-button');
+      button.addEventListener('click', createList);
+
+      function createList() {
+        const output = node.querySelector('output');
+        const list   = new MaterialList();
+        const array  = ['элемент 1', 'элемент 2', 'элемент 3', 'элемент 4', 'элемент 5'];
+        array.forEach((item, index) => list.appendChild(new MaterialListItem(index, item)));
+        output.appendChild(list);
+      }
     }
   }
 
