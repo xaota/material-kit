31ef44db9bd5f0a899ef2e942b5c325b8481dcb2
xaota <1172619+xaota@users.noreply.github.com>
2019-12-30 00:56:30 +0500

    Кнопка синтеза речи

--- pages/button/page-button.js
+++ pages/button/page-button.js
@@ -21,6 +21,7 @@ import '/material/components/button-upload-tooltip-icon/material-button-upload-t
 import '/material/components/icon/material-icon.js';
 
 import MaterialButtonVoice from '/material/components/button-voice/material-button-voice.js';
+import MaterialButtonSpeech from '/material/components/button-speech/material-button-speech.js';
 import MaterialTextarea from '/material/components/textarea/material-textarea.js';
 
 const component = Component.meta(import.meta.url, 'page-button');
@@ -37,13 +38,19 @@ const component = Component.meta(import.meta.url, 'page-button');
 
   /** */
     mount(node) {
-      const root = node.querySelector('#voice-button-demo');
+      const root1 = node.querySelector('#voice-button-demo');
 
-      const voice = root.querySelector('material-button-voice'); // первая кнопка
-      const textarea = root.querySelector('material-textarea');
+      const voice = root1.querySelector('material-button-voice'); // первая кнопка
+      const output = root1.querySelector('material-textarea');
 
-      voice.addEventListener('recognize', e => textarea.value = e.detail.text);
+      voice.addEventListener('recognize', e => output.value = e.detail.text);
 
+      const root2 = node.querySelector('#speech-button-demo');
+      const speech = root2.querySelector('material-button-speech'); // первая кнопка
+      const input = root2.querySelector('material-textarea');
+
+      input.addEventListener('change', e => speech.value = input.value);
+      speech.value = input.value; // !
       return this;
     }
   }
