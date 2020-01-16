55f0dd30045bc9d8b19b75ec20b2b3cf786f0b26
xaota <1172619+xaota@users.noreply.github.com>
2020-01-16 14:25:30 +0500

    Пример создания компонента для проекта

--- pages/default/page-default.js
+++ pages/default/page-default.js
@@ -3,6 +3,9 @@ import '../../views/page/view-page.js';
 import '../../views/article/view-article.js';
 import '../../views/source/view-source.js';
 
+import MaterialCaption from '/material/components/caption/material-caption.js';
+import MaterialText from '/material/components/text/material-text.js';
+
 const component = Component.meta(import.meta.url, 'page-default');
 /** */
   export default class DefaultPage extends Component {
