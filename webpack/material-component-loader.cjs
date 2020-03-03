6288d85fb33e648cac616d5a9e8b4978e4c55fb7
Roman Balandin <tinkerscript@yandex.ru>
2020-03-03 12:53:46 +0500

    remove unused files

--- webpack/material-component-loader.cjs
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-module.exports = function (source) {
-  const templatePath = path.resolve(this.context, 'index.html');
-  this.addDependency(templatePath);
-  return `import "${templatePath}";\n${source}`;
-};
